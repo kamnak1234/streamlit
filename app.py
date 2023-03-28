f63031d93d18da9c540fa494e3316a21997358f9
Kamini Nakte <104433119+kamnak1234@users.noreply.github.com>
2023-03-28 12:48:56 +0530

    App_PythonFile

---
+++ app.py
@@ -0,0 +1,19 @@
+
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import streamlit as st
+from predict_page import show_predict_page
+from explore_page import show_explore_page
+
+page = st.sidebar.selectbox("Explore or Predict",("Explore","Predict"))
+
+if page == "Explore":
+    show_explore_page()
+else:
+    show_predict_page()
+    
+
